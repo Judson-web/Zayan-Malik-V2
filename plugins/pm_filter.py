daa8e2139f8655c2fcc75dd82af011e2e118b1c9
‹|| Λѕωιη ||› <82755354+Aswin-TG@users.noreply.github.com>
2021-09-21 14:33:51 +0530

    Create pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -120,9 +120,14 @@ async def group(client, message):
         if files:
             for file in files:
                 file_id = file.file_id
-                filename = f"[{get_size(file.file_size)}] {file.file_name}"
+                file_name = file.file_name}"
+                file_size = get_size(file.file_size)
+                file_link = f"https://telegram.dog/{nyva}?start=subinps_-_-_-_{file_id}"
                 btn.append(
-                    [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=subinps_-_-_-_{file_id}")]
+                    [
+                      InlineKeyboardButton(text=f"{file_name}", url=f"{file_link}")
+                      InlineKeyboardButton(text=f"{file_size}", url=f"{file_link}")
+                    ]
                 )
         else:
             return
