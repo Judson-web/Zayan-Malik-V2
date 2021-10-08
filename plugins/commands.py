4e11b3ca67f344a9c480b3cbbe15dc7e78482165
Peace_fighter_TG <90506148+PeacefighterTG@users.noreply.github.com>
2021-10-08 23:23:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import random
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
